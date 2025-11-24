5a6e9aaa40052f13132c6398aac9511fefb70c1d
AgusSanticchia <santicchiaagus85@gmail.com>
2025-11-24 17:47:43 -0300

    Add environment configuration files and dotenv dependency for better management of application settings

--- dream-shop/src/main/java/com/agus/dreamshop/DreamShopApplication.java
+++ dream-shop/src/main/java/com/agus/dreamshop/DreamShopApplication.java
@@ -1,13 +1,24 @@
 package com.agus.dreamshop;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DreamShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DreamShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
 
+        
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+
+        SpringApplication.run(DreamShopApplication.class, args);
+    }
 }
